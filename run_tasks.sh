3d9b860acc38d62daced7d2ab602595c517df533
Ray Victor <ray7551@gmail.com>
2023-12-29 17:56:18 +0800

    gas adjust

--- run_tasks.sh
+++ run_tasks.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 process_count=${1:-1}
-tick=${2:-neutron}
-max_gas=${3:-150}
+tick=${2:-quark}
+max_gas=${3:-91}
 echo "process count: $process_count"
 echo "tick: $tick"
+
+# read -p "Enter Y to continue: " input
+# if [[ $input == "Y" || $input == "y" ]]; then
+#     echo "Continuing..."
+# else
+#     echo "Bye."
+#     exit
+# fi
 
 while true; do
     # get fees from mempool API
@@ -12,21 +20,21 @@ while true; do
     echo "halfHourFee: $halfHourFee"
 
     if [[ -z "$halfHourFee" || ! "$halfHourFee" =~ ^[0-9]+$ ]]; then
-	echo "use default gas"
+	    echo "use default gas"
         gas=$max_gas
-    elif [ "$halfHourFee" -gt $max_gas ]; then
-	echo "gas too high, use default gas"
+    elif [ $((halfHourFee + 2)) -gt $max_gas ]; then
+	    echo "gas too high, use default gas"
         gas=$max_gas
     else
-        gas=$halfHourFee
+        gas=$((halfHourFee + 2))
     fi
-    echo "curretn gas: $gas"
+    echo "set gas: $gas"
 
-    name=$(echo "$tick-$(date +'%y-%m-%d-%H:%M')")
+    name=$(echo "$tick-$(date +'%m-%d-%H:%M')")
     
     pm2 start "yarn cli mint-dft $tick --disablechalk --satsbyte=$gas" -i $process_count --name $name
 
-    sleep 600
+    sleep 900
 
     pm2 delete $name 
 done
